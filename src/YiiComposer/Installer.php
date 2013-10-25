8afe692766c6d0dc8753d0cc7c353449099a5339
Mihail <mihail.kucher@gmail.com>
2013-10-25 14:43:23 +0300

    fix

--- src/YiiComposer/Installer.php
+++ src/YiiComposer/Installer.php
@@ -17,118 +17,84 @@ use Composer\Installer\LibraryInstaller;
 
 class Installer extends LibraryInstaller
 {
-    protected $yiiFrameworkName = 'yiisoft/yii';
-    protected $yiiDefaulTypes = array();
-    protected $yiiOptionTypes = array();
+    protected $yiiPaths = array();
 
 
     public function __construct(IOInterface $io, Composer $composer, $type = 'library', Filesystem $filesystem = null){
-        $this->yiiDefaulTypes = array(
+        $this->yiiPaths = array(
             'module' => '{vendor}'.DIRECTORY_SEPARATOR.'modules'.DIRECTORY_SEPARATOR.'{name}',
             'extension' => '{vendor}'.DIRECTORY_SEPARATOR.'extensions'.DIRECTORY_SEPARATOR.'{name}',
-            'framework' => '{vendor}'.DIRECTORY_SEPARATOR.'framework'
+            'yiisoft/yii' => '{vendor}'.DIRECTORY_SEPARATOR.'framework'
         );
 
         if ($composer->getPackage()) {
             $extra = $composer->getPackage()->getExtra();
 
-
-            /*if(!empty($extra['yiicomposer-framework'])){
-                $this->yiiFrameworkName = $extra['yiicomposer-framework'];
-            }*/
-
             if(!empty($extra['yiicomposer-paths'])){
-                $this->yiiOptionTypes = $extra['yiicomposer-paths'];
+                $this->yiiPaths = array_merge($this->yiiPaths, $extra['yiicomposer-paths']);
             }
-
         }
 
         parent::__construct($io, $composer, $type, $filesystem);
     }
 
 
-    protected function isYii($type, $bool=true){
+    protected function yiiPackageInfo($type){
         $type = strtolower($type);
 
         if($type == 'yii-framework'){
-            if($bool)
-                return true;
             return array('type' => 'framework', 'name' => '');
         }
 
         if(preg_match('#yii-([^-]*)-(.*)#i', $type, $m)){
-            if($bool)
-                return true;
             return array('type' => $m[1], 'name' => $m[2]);
         }
 
         return false;
     }
 
-    protected function yiiTypePath($type, $name=""){
+    protected function getYiiPackageBasePath(PackageInterface $package){
+
+        $packageName = $package->getName();
+        $type = 'empty';
+        $name = '';
+        $path = false;
+
+        $info = $this->yiiPackageInfo($package->getType());
+        if(!empty($info)){
+            $type = $info['type'];
+            $name = $info['name'];
+            $path = "{vendor}".DIRECTORY_SEPARATOR."{type}".DIRECTORY_SEPARATOR."{name}";
+        }
+
+        if(isset($this->yiiPaths[$packageName])){
+            $path = $this->yiiPaths[$packageName];
+        }elseif(isset($this->yiiPaths[$type]) $$ $type !== false){
+            $path = $this->yiiPaths[$type];
+        }
+
+        if($path === false)
+            return false;
+
+        $path = str_replace('/', DIRECTORY_SEPARATOR, $path);
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
+        $packageName = str_replace('/', DIRECTORY_SEPARATOR, $packageName);
 
         $this->initializeVendorDir();
-
-        $path = "{vendor}".DIRECTORY_SEPARATOR."{type}".DIRECTORY_SEPARATOR."{name}";
-        if(isset($this->yiiOptionTypes[$type])){
-            $path = str_replace('/', DIRECTORY_SEPARATOR, $this->yiiOptionTypes[$type]);
-        }elseif(isset($this->yiiDefaulTypes[$type])){
-            $path = $this->yiiTypes[$type];
-        }
-        $info = array("{vendor}" => $this->vendorDir, "{type}" => $type, "{name}" => $name);
+        $info = array("{vendor}" => $this->vendorDir, "{type}" => $type, "{package}" => $packageName, "{name}" => $name);
         $path = strtr($path, $info);
+
         return rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
     }
 
     /**
      * {@inheritDoc}
      */
-    public function getInstallPath(PackageInterface $package)
-    {
-        if($package->getName() == $this->yiiFrameworkName){
-            return $this->yiiTypePath('framework');
-        }
-
-        $info = $this->isYii($package->getType(), false);
-        if($info === false)
+    protected function getPackageBasePath(PackageInterface $package){
+        $path = $this->getYiiPackageBasePath($package);
+        if($path === false)
             return parent::getInstallPath($package);
 
-        return $this->yiiTypePath($info['type'], $info['name']);
+        return $path;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        if($package->getName() == $this->yiiFrameworkName){
-            $info['type'] = 'framework';
-            $info['name'] = '';
-        }else{
-            $info = $this->isYii($package->getType(), false);
-            if($info === false)
-                return parent::uninstall($repo, $package);
-        }
-
-
-        if (!$repo->hasPackage($package)) {
-            throw new \InvalidArgumentException('Package is not installed: '.$package);
-        }
-
-        $repo->removePackage($package);
-
-        $installPath = $this->yiiTypePath($info['type'], $info['name']);
-        $this->io->write(sprintf('Deleting %s - %s', $installPath, $this->filesystem->removeDirectory($installPath) ? '<comment>deleted</comment>' : '<error>not deleted</error>'));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($packageType)
-    {
-		if($this->isYii($packageType))
-            return true;
-
-        return parent::supports($packageType);
-	}
 }
